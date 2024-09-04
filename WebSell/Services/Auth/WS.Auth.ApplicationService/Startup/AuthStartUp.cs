db3a372d10c64d5a13b5cf2a712508367645319d
DucThanh <phoducthanh@gmail.com>
2024-09-04 13:01:01 +0700

    add ConfigureAuth

---
+++ WebSell/Services/Auth/WS.Auth.ApplicationService/Startup/AuthStartUp.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WS.Auth.Infrastructures;
+using WS.Shared.Constant.Database;
+
+namespace WS.Auth.ApplicationService.Startup
+{
+    public static class AuthStartup
+    {
+        public static void ConfigureAuth(this WebApplicationBuilder builder, string? assemblyName)
+        {
+            builder.Services.AddDbContext<AuthDbContext>(
+                options =>
+                {
+                    options.UseSqlServer(
+                        builder.Configuration.GetConnectionString("Default"),
+                        options =>
+                        {
+                            options.MigrationsAssembly(assemblyName);
+                            options.MigrationsHistoryTable(
+                                DbSchema.TableMigrationsHistory,
+                                DbSchema.Auth
+                            );
+                        }
+                    );
+                },
+                ServiceLifetime.Scoped
+            );
+
+            //builder.Services.AddScoped<>();
+        }
+    }
+}
---
+++ WebSell/Services/Shared/WS.Shared.Constant/Permission/PermissionKeys.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WS.Shared.Constant.Permission
+{
+    public static class PermissionKeys
+    {
+        public const string AuthCreateUser = "auth_create_user";
+    }
+}
